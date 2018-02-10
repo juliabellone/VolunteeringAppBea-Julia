08d9c0754e63fba74fae7cdfa12121534c9e0695
Beatriz <Beatriz@MacBook-Pro-de-Beatriz.local>
2018-02-10 16:20:21 +0100

    set up2

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const path = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
+const index = require('./routes/index');
+const users = require('./routes/users');
+const expressLayouts = require('express-ejs-layouts');
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
+
+app.set("layout extractScripts", true) // see Documentation
+app.set("layout extractStyles", true) // see Documentation
+app.set("layout extractMetas", true) // see Documentation
+app.set('layout', 'layouts/main'); // custom layout
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,13 +27,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressLayouts);
 
 app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
