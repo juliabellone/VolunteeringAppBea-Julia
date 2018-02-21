726f3936b40dfdec4d26f2f21a4e17d780bdb492
julia <juliabellone@gmail.com>
2018-02-21 20:10:45 +0100

    populate no funciona

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const flash = require('connect-flash');
 
 const User = require('../models/user');
 const Offer = require('../models/offer');
+const Ong = require('../models/ong');
 
 /* GET users listing. */
 // router.get('/:username', (req, res, next) => {
@@ -26,9 +27,10 @@ router.get('/opportunities', (req, res, next) => {
   console.log(userInterests);
   const offers = [];
   Offer.find({ category: { $in: userInterests } })
-    .populate('_ong', 'name')
+    
     // <span><%= offer._ong.name %></span> el populate no funciona
     .then((offers) => {
+      .populate('_ong');
       res.render('user/opportunities', { offers });
     })
     .catch((err) => {
