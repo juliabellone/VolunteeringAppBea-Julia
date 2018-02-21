cf9891b1f9050a5d14463bd7810bf828f8d54086
julia <juliabellone@gmail.com>
2018-02-21 19:16:30 +0100

    offers for user work

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const flash = require('connect-flash');
 
 const User = require('../models/user');
+const Offer = require('../models/offer');
 
 /* GET users listing. */
 // router.get('/:username', (req, res, next) => {
@@ -18,6 +19,23 @@ router.get('/profile', (req, res, next) => {
     .catch((err) => {
       next(err);
     });
+});
+
+router.get('/opportunities', (req, res, next) => {
+  const userInterests = req.user.interests;
+  console.log(userInterests);
+  const offers = [];
+  Offer.find({ category: { $in: userInterests } })
+    .populate('_ong', 'name')
+    // <span><%= offer._ong.name %></span> el populate no funciona
+    .then((offers) => {
+      res.render('user/opportunities', { offers });
+    })
+    .catch((err) => {
+      next(err);
+    });
+  console.log(offers);
+
 });
 
 module.exports = router;
