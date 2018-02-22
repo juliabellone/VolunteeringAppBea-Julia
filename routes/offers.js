bb8984151effc05edbe881b4a3f1ba4da68f7667
julia <juliabellone@gmail.com>
2018-02-22 15:18:21 +0100

    no funciona jquery

--- routes/offers.js
+++ routes/offers.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
 
 const User = require('../models/user');
 const Offer = require('../models/offer');
@@ -8,16 +10,28 @@ const Ong = require('../models/ong');
 /* GET offers page */
 
 router.get('/:offerId', (req, res, next) => {
-  const offerId  = req.params.offerId;
+  const offerId = req.params.offerId;
   // If offer id es un id valido...
   Offer.findById(offerId, (err, offer) => {
-    console.log('hola');
-    console.log(offer);
-
     if (err) { return next(err); }
     res.render('offers/offer', { offer });
   });
 });
 
+
+//if (offerId instanceof mongoose.Types.ObjectId)  --> no funciona ????
+
+
+// // user subscribes to an offer
+router.post('/:offerId/subscribe', (req, res, next) => {
+  console.log('offersubscribe')
+  const offerId = req.params.offerId;
+  const userId = req.user.id;
+
+  User.findOneAndUpdate({_id: userId }, { $push: { offerId } }, (err, next) => {
+    if (err) { return next(err); }
+  })
+
+});
 
 module.exports = router;
